8483bb35746f0b58d32453e200d1168b63ae344c
godwynnn <miraclegodwin67@gmail.com>
2025-04-15 02:35:54 +0100

    init

--- src/components/eventsComponents/today.js
+++ src/components/eventsComponents/today.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { View, SafeAreaView, Text, FlatList, TouchableOpacity, Image, StyleSheet, Dimensions } from 'react-native'
+import { View, SafeAreaView, Text, FlatList, TouchableOpacity, Image, StyleSheet, Dimensions,ImageBackground } from 'react-native'
 import tailwind from 'twrnc'
 // import { Image } from 'react-native'
 import img1 from '../../../assets/ft1.png'
@@ -43,7 +43,19 @@ const feature_data = [
 
 ]
 
+const favourite_data=[
+  {
+    icon:'home',
+    title:'Home',
+    info:'Add a home address'
+  },
 
+  {
+    icon:'briefcase',
+    title:'Work',
+    info:'Add a work address'
+  }
+]
 
 
 
@@ -104,48 +116,74 @@ export default function Today({ navigation }) {
         />
       </View>
 
+      
       <View style={[tw `p-0 px-4 flex-0.3`,{gap:10}]}>
+
+
       <Text style={tw `font-500 text-md`}> Favourite</Text>
-        
-        <TouchableOpacity style={[tw`bg-[whitesmoke] flex-0.5 px-5 flex-row`,{alignItems:'center', borderRadius:10, justifyContent:'space-between'}]}>
-        
-          
-          <View style={[tw`flex-row`,{gap:20,justifyContent:'center',alignItems:'center'}]}>
-          <Ionicons name='home' size={25} color={'#1D1A38'} /> 
-            <View>
-            <Text style={[tw`text-[16px]`,{fontWeight:'bold',}]}>Home</Text>
-            <Text>Add a home address</Text>
-            </View>
-            
-          </View>
 
-          <View>
-          <Ionicons name='arrow-forward' size={20} color={'#1D1A38'} /> 
-          </View>
+      <FlatList
+          data={favourite_data}
+          contentContainerStyle={{gap:10}}
+          showsVerticalScrollIndicator={false}
           
+          keyExtractor={(item, index) => item.id}
+
+          renderItem={({ item }) => {
+            return (
+              <TouchableOpacity style={[tw`bg-[whitesmoke]  px-5 flex-row h-15`,{alignItems:'center', borderRadius:10, justifyContent:'space-between'}]}>
+        
+          
+                <View style={[tw`flex-row`,{gap:20,justifyContent:'center',alignItems:'center'}]}>
+                <Ionicons name={item.icon} size={25} color={'#1D1A38'} /> 
+                  <View>
+                  <Text style={[tw`text-[16px]`,{fontWeight:'bold',}]}>{item.title}</Text>
+                  <Text>{item.info}</Text>
+                  </View>
+                  
+                </View>
+
+                <View>
+                <Ionicons name='arrow-forward' size={20} color={'#1D1A38'} /> 
+                </View>
+                
             
         </TouchableOpacity>
-
-        <TouchableOpacity style={[tw`bg-[whitesmoke] flex-0.5 px-5 flex-row`,{alignItems:'center', borderRadius:10, justifyContent:'space-between'}]}>
+              )
+          }}
+        />
         
-          
-          <View style={[tw`flex-row`,{gap:20,justifyContent:'center',alignItems:'center'}]}>
-          <Ionicons name='home' size={25} color={'#1D1A38'} /> 
-            <View>
-            <Text style={[tw`text-[16px]`,{fontWeight:'bold',}]}>Home</Text>
-            <Text>Add a home address</Text>
-            </View>
-            
-          </View>
+        
 
-          <View>
-          <Ionicons name='arrow-forward' size={20} color={'#1D1A38'} /> 
-          </View>
-          
-            
-        </TouchableOpacity>
       </View>
 
+
+<View style={[tw` flex-0.4 p-4 pt-0 `,{}]}>
+
+        
+        <View style={[tw`bg-[#1D1A38] flex-0.9  mt-10 `,{borderRadius:20,position:'relative'}]}>
+          <Text style={tw`text-[#fff] font-700 text-xl mt-5 ml-2`}>Schedule Your Ride</Text>
+          <Text style={tw`text-[#fff] font-400 text-[12px] mt-1 ml-2`}> Plan ahead, ride stress-free</Text>
+          <Text style={tw`text-[#fff] font-400 text-[12px] mt-1 ml-2`}> Schedule your ride now!</Text>
+
+          <TouchableOpacity style={[tw`bg-[whitesmoke] w-[30%] mt-2 ml-2  h-[25%] flex`,{alignItems:'center', borderRadius:10,justifyContent:'center' }]}>
+        
+          
+          <Text style={tw`text-[#1D1A38] font-500 text-[12px] text-center`}> Let's go!</Text>
+            
+        </TouchableOpacity>
+
+
+                <Image source={require('../../../assets/ft1.png')} 
+                resizeMode="contain" style={{width:'80%',height:'100%',position:'absolute',right:-50}} />
+                      
+                
+
+
+        </View>
+
+
+      </View>
 
 
     </SafeAreaView>
